7c48bf399a4879cf86a87d760fc92ee6f2e7a8fe
Javier Martin Romero <162146674+xavimrg@users.noreply.github.com>
2025-05-13 12:42:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import makeWASocket, { useMultiFileAuthState } from '@whiskeysockets/baileys';
-import dotenv from 'dotenv';
+import { makeWASocket, useMultiFileAuthState } from "@whiskeysockets/baileys";
 import { startSchedule } from './scheduler.js';
-
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 const startBot = async () => {
-  const { state, saveCreds } = await useMultiFileAuthState('auth_info');
+  const { state, saveCreds } = await useMultiFileAuthState('./auth');
   const sock = makeWASocket({
     auth: state,
-    printQRInTerminal: true
+    printQRInTerminal: true,
   });
 
   sock.ev.on('creds.update', saveCreds);
@@ -17,7 +16,7 @@ const startBot = async () => {
     const { connection, lastDisconnect } = update;
     if (connection === 'open') {
       console.log('✅ Bot conectado correctamente a WhatsApp.');
-      startSchedule(sock); // Lanzamos las tareas programadas
+      startSchedule(sock); // Lanzamos tareas programadas
     } else if (connection === 'close') {
       console.log('❌ Conexión cerrada. Reconectando...');
       startBot();
