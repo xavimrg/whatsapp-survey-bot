ffdc5104769f2d1e2579351410327e637d517b54
Javier Martin Romero <162146674+xavimrg@users.noreply.github.com>
2025-05-21 12:19:25 +0200

    Update index.js mas pescado

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import { makeWASocket, DisconnectReason } from 'baileys';
-import P from 'pino';
-
-// Importa useSingleFileAuthState de su módulo específico
 import { useSingleFileAuthState } from '@adiwajshing/baileys/lib/Authentication/index.js';
+import P from 'pino';
 
 const SESSION_FILE_PATH = process.env.RAILWAY ? '/app/session/session.json' : './session/session.json';
 
 const { state, saveState } = useSingleFileAuthState(SESSION_FILE_PATH);
-
 
 async function startSock() {
   const sock = makeWASocket({
